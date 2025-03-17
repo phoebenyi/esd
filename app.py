236200b464d165e839b613885674530c55968697
phoebe neo <phoebe.neo.2023@scis.smu.edu.sg>
2025-03-17 22:52:23 +0800

    added endpoints to firebase

--- app.py
+++ app.py
@@ -10,6 +10,28 @@ CORS(app, origins="http://localhost:3000")
 def test():
     return 'ALIVE!'
 
+@app.route("/all", methods=["GET"])
+def get_all_posts():
+    bufferjson = {}
+    post_ref = db.collection("Posts").stream()
+    for doc in post_ref:
+        bufferjson[doc.id] = doc.to_dict()
+    return jsonify(bufferjson)
+
+@app.route("/<userId>", methods=["GET"])
+def get_user_posts(userId):
+    bufferjson = {}
+    post_ref = db.collection("Posts").where("userId", "==", userId).stream()
+    for doc in post_ref:
+        bufferjson[doc.id] = doc.to_dict()
+    return jsonify(bufferjson)
+
+def get_all_posts():
+    bufferjson = {}
+    post_ref = db.collection("Posts").stream()
+    for doc in post_ref:
+        bufferjson[doc.id] = doc.to_dict()
+    return jsonify(bufferjson)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000)
